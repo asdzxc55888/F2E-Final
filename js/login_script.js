641b883c88515be1894d6938b608cc6c77ebd3d1
asdzxc55888 <asdzxc55888@gmail.com>
2018-06-21 18:49:11 +0800

    FB

--- js/login_script.js
+++ js/login_script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     const $btnSignOut = $('#btnSignOut');
     const $signInfo = $('#sign-info');
     const $btnGoogleSingIn = $('#btnGoogleSingIn');
-    const $btnFBSingIn=$('#btnFBSingIn');
+    const $btnFBSingIn = $('#btnFBSingIn');
 
     var user = firebase.auth().currentUser;
     if (user) {
@@ -49,12 +49,12 @@ $(document).ready(function () {
         });
     });
 
-    $btnSignOut.click(function(){
+    $btnSignOut.click(function () {
         firebase.auth().signOut();
         console.log('LogOut');
         $signInfo.html('No one login...');
         $message.html('');
-      });
+    });
 
     // Listening Login User
     firebase.auth().onAuthStateChanged(function (user) {
@@ -83,7 +83,7 @@ $(document).ready(function () {
         }
     });
 
-    $btnFBSingIn.click(function(){
+    $btnFBSingIn.click(function () {
         var provider = new firebase.auth.FacebookAuthProvider();
         firebase.auth().signInWithRedirect(provider);
         console.log('進來了好棒2');
@@ -94,6 +94,25 @@ $(document).ready(function () {
         firebase.auth().signInWithRedirect(provider);
         console.log('進來了好棒');
     });
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
 
 
 });
