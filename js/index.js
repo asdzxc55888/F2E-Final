01f564b7968edac364e15e01d820fa77945f8dd6
t105590005 <t22923200@gmail.com>
2018-06-24 00:09:07 +0800

    輪播

--- js/index.js
+++ js/index.js
@@ -4,17 +4,9 @@ $(function(){
     $item.height($wHeight); 
     $item.addClass('full-screen');
 
-    $('.carousel img').each(function() {
-      var $src = $(this).attr('src');
-      var $color = $(this).attr('data-color');
-      $(this).parent().css({
-        'background-image' : 'url(' + $src + ')',
-        'background-color' : $color
-      });
-      $(this).remove();
-    });
+
     $('.carousel').carousel({
-        interval: 3000,
+        interval: 4000,
         ride:true
       }) 
 });
