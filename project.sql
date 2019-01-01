822ad116c541be161ec6330d5539fb8920651d95
a105590015 <t105590015@ntut.org.tw>
2019-01-01 23:47:09 +0800

    更新初始化MySQL

--- project.sql
+++ project.sql
@@ -71,6 +71,7 @@ Quantity INT NOT NULL,
 PRIMARY KEY (Order_ID, Product_ID),
 FOREIGN KEY (Order_ID) REFERENCES orders(id));
 
+//初始化商品資訊
 INSERT INTO Product
 VALUES		(001, 180, "杏仁果牛奶糖", 100, "img/candy1.jpg");
 INSERT INTO Product
@@ -86,6 +87,7 @@ VALUES		(006, 200, "鮮乳手做餅(乳酪)", 100, "img/cookie2.jpg");
 INSERT INTO Product
 VALUES		(007, 200, "鮮乳手做餅(原味)", 100, "img/cookie3.jpg");
 
+//初始化商品類別資訊
 INSERT INTO Product_Category
 VALUES		("糖果", 001);
 INSERT INTO Product_Category
@@ -101,6 +103,7 @@ VALUES		("餅乾", 006);
 INSERT INTO Product_Category
 VALUES		("餅乾", 007);
 
+//初始化設施資訊
 INSERT INTO Farm_Information
 VALUES		(001, "腳踏車", "正常開放", "img/img_bicycle.jpg");
 INSERT INTO Farm_Information
@@ -114,11 +117,16 @@ VALUES		(005, "體驗餵牛", "正常開放", "img/img_feed_cow.jpg");
 INSERT INTO Farm_Information
 VALUES		(006, "體驗騎牛", "正常開放", "img/img_ride_cow.jpg");
 
+//初始化使用者資訊
 INSERT INTO user
-VALUES		(1, "test@gmail.com", md5("123"), "0965010920", "何韋辰", "123", "2001-11-00", 1, "好棒"), (2, "user@gmail.com", md5("456"), "0800092000", "何媽媽", "456", "2001-02-28", 0, "bang");
+VALUES		(1, "test@gmail.com", md5("123"), "0965010920", "何韋辰", "123", "2001-11-00", 1, "好棒");
+INSERT INTO user
+VALUES      (2, "user@gmail.com", md5("456"), "0800092000", "何媽媽", "456", "2001-02-28", 0, "bang");
 
+//初始化訂單資訊
 INSERT INTO orders 
 VALUES (1,0,NULL,NULL,NULL,1,0);
 
+//初始化訂單內容資訊
 INSERT INTO order_product
 VALUES (1,1,4), (1,2,1);
